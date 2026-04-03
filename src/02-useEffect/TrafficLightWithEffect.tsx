69ccf84ea5f78940a2896ede638f739a3a250c4e
Dell_JESMQZ <jesmqz@gmail.com>
2026-04-03 14:55:16 -0400

    useeffect secundarios

--- src/02-useEffect/TrafficLightWithEffect.tsx
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -17,9 +17,9 @@ export const TrafficLightWithEffect = () => {
 
   useEffect(() => {
     if (countdown === 0) return;
-    console.log('useEffect');
+    // console.log('useEffect');
     const intervalId = setInterval(() => {
-      console.log(countdown);
+      // console.log(countdown);
       setCountdown((prev) => prev -1 );
     }, 1000);
     return () => {
@@ -28,15 +28,40 @@ export const TrafficLightWithEffect = () => {
     }
   }, [countdown]);
 
+  useEffect(() => {
+    if (countdown > 0) return;
+
+    setCountdown(5);
+    if (light === 'red') {
+      setLight('green')
+      return;
+    }
+    if (light === 'yellow') {
+      setLight('red')
+      return;
+    }
+    if (light === 'green') {
+      setLight('yellow')
+      return;
+    }
+    
+  }, [countdown, light]);
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
         <h1 className="text-white text-3xl font-thin">Semaforo con useeffect</h1>
         <h2 className="text-white text-xl">Countdown: { countdown }</h2>
 
-        <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-red-500'} rounded-full`}></div>
-        <div className={`w-32 h-32 ${ light === 'yellow' ? colors[light] : 'bg-yellow-500'} rounded-full`}></div>
-        <div className={`w-32 h-32 ${ light === 'green' ? colors[light] : 'bg-green-500' } rounded-full`}></div>
+        <div className="w-64 bg-gray-700 rounded-full h-2">
+          <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
+            style={{ width: `${(countdown / 5) * 100 }%`}}>
+
+          </div>
+        </div>
+        <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+        <div className={`w-32 h-32 ${ light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+        <div className={`w-32 h-32 ${ light === 'green' ? colors[light] : 'bg-gray-500' } rounded-full`}></div>
       </div>
     </div>
   );
