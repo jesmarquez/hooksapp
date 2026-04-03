a7e851404faae26f2193afb009816c07880ad35d
Dell_JESMQZ <jesmqz@gmail.com>
2026-04-03 12:32:04 -0400

    useState color light

---
+++ src/01-useState/TrafficLight.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export const TrafficLight = () => {
+
+  const color = {
+    'red': 'bg-red-500 animate-pulse',
+    'yellow': 'bg-yellow-500 animate-pulse',
+    'green': 'bg-green-500 animate-pulse'
+  }
+  const [light, setLight] = useState('red');
+
+  const handleColorChange = (color) => {
+
+    setLight( (prev) => {
+      console.log(prev);
+      return color;
+    });
+  }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+        <div className={`w-32 h-32 ${ light === 'red' ? color[light] : 'bg-red-500'} rounded-full`}></div>
+        <div className={`w-32 h-32 ${ light === 'yellow' ? color[light] : 'bg-yellow-500'} rounded-full`}></div>
+        <div className={`w-32 h-32 ${ light === 'green' ? color[light] : 'bg-green-500' } rounded-full`}></div>
+
+        {/* Botón para cambiar el estado de la luz */}
+        <div className="flex gap-2">
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={ handleColorChange}>
+            Rojo
+          </button>
+          <button
+            className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={ handleColorChange }>
+            Amarillo
+          </button>
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={ handleColorChange }>
+            Verde
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
