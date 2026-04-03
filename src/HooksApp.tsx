a498542f3c37ab3d574c72a26c697b9946abb0c9
Dell_JESMQZ <jesmqz@gmail.com>
2026-04-03 11:28:05 -0400

    tailwind css

--- src/HooksApp.tsx
+++ src/HooksApp.tsx
@@ -2,6 +2,10 @@
 
 export const HooksApp = () => {
   return (
-    <div>Hola guys</div>
+    <div className="bg-gradient">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
