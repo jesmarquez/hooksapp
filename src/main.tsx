8502e04db2d5c1868475a0bb881f22fbcd4712cb
Dell_JESMQZ <jesmqz@gmail.com>
2026-04-08 21:09:16 -0400

    useMemo

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { MemoHook } from './06-memos/MemoHook'
-import './index.css'
+// import { MemoHook } from './06-memos/MemoHook'
 // import { HooksApp } from './HooksApp'
 // import { TrafficLight } from './01-useState/TrafficLight'
 // import { TasksApp } from './05-useReducer/TaskApp'
@@ -10,7 +9,8 @@ import './index.css'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
-
+import './index.css'
+import { MemoCounter } from './06-memos/MemoCounter'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -22,6 +22,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <FocusScreen/> */}
     {/* <TasksApp/> */}
     {/* <ScrambleWords /> */}
-    <MemoHook />
+    {/* <MemoHook /> */}
+    <MemoCounter />
   </StrictMode>,
 )
